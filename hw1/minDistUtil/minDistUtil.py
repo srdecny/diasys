a033fbd36e2de18023b0ad545f036f445e70a087
Vojtěch Srdečný <vojtech.srdecny@orchitech.cz>
2019-02-23 15:35:41 +0100

    Add more args.

--- hw1/minDistUtil/minDistUtil.py
+++ hw1/minDistUtil/minDistUtil.py
@@ -4,8 +4,10 @@ from levenshtein import Levenshtein
 parser = argparse.ArgumentParser(description="Find the Levenshtein distance between two strings.")
 parser.add_argument("string1", help="First string.")
 parser.add_argument("string2", help="Second string.")
+parser.add_argument("-d", "--delimiter", help="Word delimiter. Default value: space")
+parser.add_argument("-A", "--alignment", help="Print alignment.", action="store_true")
 
 args = parser.parse_args()
 
-Levenshtein(args.string1, args.string2)
+Levenshtein(args.string1, args.string2, args.delimiter, args.alignment)
 
