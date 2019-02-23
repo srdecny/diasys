90369a7143c454e36b962d09054a19c9d74abdd1
Vojtěch Srdečný <vojtech.srdecny@orchitech.cz>
2019-02-23 14:14:36 +0100

    Working distance.

--- hw1/minDistUtil/minDistUtil.py
+++ hw1/minDistUtil/minDistUtil.py
@@ -1,4 +1,5 @@
 import argparse
+from levenshtein import Levenshtein
 
 parser = argparse.ArgumentParser(description="Find the Levenshtein distance between two strings.")
 parser.add_argument("string1", help="First string.")
@@ -6,5 +7,5 @@ parser.add_argument("string2", help="Second string.")
 
 args = parser.parse_args()
 
-print(args.string1)
-print(args.string2)
+Levenshtein(args.string1, args.string2)
+
